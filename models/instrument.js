238e0a9674702c9b502ba4df6e71969774defc3e
Ellie <eagyeman1234@gmail.com>
2023-09-25 00:08:40 -0400

    write out instrument model

---
+++ models/instrument.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const instrumentSchema = new Schema({
+  name: { 
+    type: String, 
+    enum: ['Piano', 'Bass Guitar', 'Electric Guitar', 'Acoustic Guitar', 'Drums', 'Flute'], 
+    required: true},
+}, {
+  timestamps: true
+})
+
+
+
+const Instrument = mongoose.model('Instrument', instrumentSchema)
+
+export {
+  Instrument
+}
